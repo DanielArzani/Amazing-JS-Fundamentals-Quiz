66bb4195085fd08977f616066aa54ee723c2b5b3
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-11-12 22:21:55 -0500

    More updates + questions can be cycled through until specified end

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -18,38 +18,42 @@ const startButtonEl = document.querySelector("#start-quiz");
 let currentQuestions = {};
 // List of unique questions left
 let availableQuestions = [];
+// what question the player is currently on
+let questionCounter = 0;
+// score
+// let score = 0;
 
 //* Questions
 let questions = [
   {
     question: "Commonly used data types do not include___",
-    option1: "Boolean",
-    option2: "Number",
-    option3: "String",
-    option4: "JavaType",
-    answer: 1,
+    option1: "1. Boolean",
+    option2: "2. Number",
+    option3: "3. String",
+    option4: "4. JavaType",
+    answer: 4,
   },
   {
     question: "Arrays in JavaScript can be used to store___?",
-    option1: "Number and strings",
-    option2: "Other arrays",
-    option3: "Booleans",
-    option4: "All of the above",
-    answer: 3,
+    option1: "1. Number and strings",
+    option2: "2. Other arrays",
+    option3: "3. Booleans",
+    option4: "4. All of the above",
+    answer: 4,
   },
   {
     question: "The condition in an if/else statement is enclosed in an___",
-    option1: "Quotes",
-    option2: "Curly Brackets",
-    option3: "Parenthesis",
-    option4: "Square Brackets",
-    answer: 4,
+    option1: "1. Quotes",
+    option2: "2. Curly Brackets",
+    option3: "3. Parenthesis",
+    option4: "4. Square Brackets",
+    answer: 2,
   },
 ];
 
 //* Functions
 // When this function is called it will hide the current section and show the next one
-let counter = 0;
+let sectionCounter = 0;
 const nextSection = function () {
   // Each section has been given an index number
   let currentSection = [
@@ -59,10 +63,10 @@ const nextSection = function () {
     highScoresSection,
   ];
   // When the start button is pressed the first section will hide
-  currentSection[counter].classList.add("hide");
-  counter++;
+  currentSection[sectionCounter].classList.add("hide");
+  sectionCounter++;
   // The counter will increment and cause the next section to appear
-  currentSection[counter].classList.remove("hide");
+  currentSection[sectionCounter].classList.remove("hide");
   // This will make availableQuestions into a full copy of the our questions array
   availableQuestions = [...questions];
   // Calling function that displays our questions
@@ -70,6 +74,10 @@ const nextSection = function () {
 };
 
 function getNewQuestions() {
+  if (availableQuestions.length === 0) {
+    alert("done");
+  }
+  questionCounter = 0;
   // Creates a variable that stores the index position of random available questions
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   // The question currently being asked will be randomized each time
@@ -81,8 +89,23 @@ function getNewQuestions() {
     const number = option.dataset["number"];
     option.innerText = currentQuestions["option" + number];
   });
+
+  // Removes a repeated question so it can't be used anymore
+  availableQuestions.splice(questionIndex, 1);
 }
 
 //* Event Listeners
 // When start button is pressed, the quiz will start
 startButtonEl.addEventListener("click", nextSection);
+
+// When answer is clicked on, right or wrong will be displayed, time from timer will be subtracted and next question will show
+quizAnswers.forEach(function (option) {
+  // Listens for a click event on one of the answers
+  option.addEventListener("click", function (e) {
+    // Stores the answer that is clicked on in a variable called choice
+    const choice = e.target;
+    // Stores the dataset of the answer that was clicked on
+    const answer = choice.dataset["number"];
+    getNewQuestions();
+  });
+});
